055cea36078a7620cc269065b3dc71a170e336a0
gfanton <guilhen.fanton@prestashop.com>
2015-12-22 17:11:56 +0100

    [ADD] bootstrap support

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp    = require('gulp');
+var sass    = require('gulp-sass');
+var replace = require('gulp-replace');
+var print   = require('gulp-print');
+var rootDir = __dirname;
+var nodeModulesDir = rootDir + '/node_modules';
 
 gulp.task('sass', function () {
+    var bootstrapDir = '@import "' + nodeModulesDir + '/bootstrap/scss/$2"';
     gulp.src(['./scss/application.scss'])
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+        .pipe(replace(/@import\s+['"](bootstrap)\/(.+)['"]/g, bootstrapDir)) // get bootsrap root folder
+
+        .pipe(sass() .on('error', sass.logError)) // build sass
+
+        .pipe(gulp.dest('./css')); // export css
 });
 
 gulp.task('sass:watch', function () {
     gulp.watch('./scss/**/*.scss', ['sass']);
-});
+i});

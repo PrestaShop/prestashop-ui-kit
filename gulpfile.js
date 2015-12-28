85a7f59f536c301264da97ca087b78e02ec4c141
melvin <melvin.moustaid@prestashop.com>
2015-12-28 17:58:26 +0100

    minify / custom css

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,16 @@
 'use strict';
 
-var gulp    = require('gulp');
-var sass    = require('gulp-sass');
-var replace = require('gulp-replace');
-var rootDir = __dirname;
+var gulp           = require('gulp');
+var sass           = require('gulp-sass');
+var replace        = require('gulp-replace');
+var rootDir        = __dirname;
 var nodeModulesDir = rootDir + '/node_modules';
 
 gulp.task('sass', function () {
     var bootstrapDir = '@import "' + nodeModulesDir + '/bootstrap/scss/$2"';
     gulp.src(['./scss/application.scss'])
-        .pipe(replace(/@import\s+['"](bootstrap)\/(.+)['"]/g, bootstrapDir)) // get bootsrap root folder
 
         .pipe(sass() .on('error', sass.logError)) // build sass
-
         .pipe(gulp.dest('./css')); // export css
 });
 

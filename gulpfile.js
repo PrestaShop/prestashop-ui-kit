b2949a04650363ad8eb1a4a4ce319282a1aa213d
melvin <melvin.moustaid@prestashop.com>
2015-12-29 11:27:08 +0100

    Add gulp-css-purge && gulp-minify-css

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,17 @@
 var gulp           = require('gulp');
 var sass           = require('gulp-sass');
 var replace        = require('gulp-replace');
-var purge = require('gulp-css-purge');
-var dedupe = require('gulp-dedupe');
-var nano = require('gulp-cssnano');
-
+var purge          = require('gulp-css-purge');
+var minifyCss      = require('gulp-minify-css');
 var rootDir        = __dirname;
 var nodeModulesDir = rootDir + '/node_modules';
 
 gulp.task('sass', function () {
     var bootstrapDir = '@import "' + nodeModulesDir + '/bootstrap/scss/$2"';
     gulp.src(['./scss/application.scss'])
-
         .pipe(sass() .on('error', sass.logError)) // build sass
         .pipe(purge())
-        .pipe(dedupe())
+        .pipe(minifyCss())
         .pipe(gulp.dest('./css')); // export css
 });
 

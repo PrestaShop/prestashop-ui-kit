639454b09b95e7cd8d11367cc5a4b1988e921706
melvin <melvin.moustaid@prestashop.com>
2015-12-29 11:03:11 +0100

    Maj

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@
 var gulp           = require('gulp');
 var sass           = require('gulp-sass');
 var replace        = require('gulp-replace');
+var purge = require('gulp-css-purge');
+var dedupe = require('gulp-dedupe');
+var nano = require('gulp-cssnano');
+
 var rootDir        = __dirname;
 var nodeModulesDir = rootDir + '/node_modules';
 
@@ -11,6 +15,8 @@ gulp.task('sass', function () {
     gulp.src(['./scss/application.scss'])
 
         .pipe(sass() .on('error', sass.logError)) // build sass
+        .pipe(purge())
+        .pipe(dedupe())
         .pipe(gulp.dest('./css')); // export css
 });
 

c81a7155d2f06ffff09e989d701d1fef0cd041b3
melvin <melvin.moustaid@prestashop.com>
2016-01-18 13:06:21 +0100

    [Update] doc

--- js/prestakit.js
+++ js/prestakit.js
@@ -10,4 +10,22 @@ $(function () {
     };
     $('.tooltip-error').tooltip(options);
     $('[data-toggle="tooltip"]').tooltip();
+
+    $(function() {
+        $( ".btn" ).click(function() {
+            $( ".btn" ).addClass( "onclic", 250, validate);
+        });
+
+        function validate() {
+            setTimeout(function() {
+                $( ".btn" ).removeClass( "onclic" );
+                $( ".btn" ).addClass( "validate", 450, callback );
+            }, 2250 );
+        }
+        function callback() {
+            setTimeout(function() {
+                $( ".btn" ).removeClass( "validate" );
+            }, 1250 );
+        }
+    });
 });

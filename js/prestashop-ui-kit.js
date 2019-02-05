909aa77ce3f6527cc3916334fedeea11f4896136
Tomas Ilginis <tilginis@gmail.com>
2019-02-05 13:52:44 +0200

    adds possibility to see uploaded file name- on multiple files upload it displays number of files and title passed from data attribute

--- js/prestashop-ui-kit.js
+++ js/prestashop-ui-kit.js
@@ -118,6 +118,27 @@ $.fn.pstooltip = $.fn.tooltip;
       });
     });
   }
+  
+  var initInputFileChangeEvent = function () {
+    jQuery('.custom-file input').on('change', function () {
+      var $input = $(this);
+      var files = $input[0].files;
+      var filesCount = files.length;
+      var $label = $input.next('label');
+
+      if (1 === filesCount) {
+        $label.text(files[0].name);
+      } else if (1 < filesCount) {
+        var multipleFilesTranslationText = $input.attr('data-multiple-files-text');
+
+        if (typeof multipleFilesTranslationText !== 'undefined') {
+          $label.text(filesCount + ' ' + multipleFilesTranslationText);
+        } else {
+          $label.text(filesCount + ' files');
+        }
+      }
+    })
+  }
 
   var initToolTips = function() {
     jQuery('.tooltip-error').pstooltip({
@@ -165,6 +186,7 @@ $.fn.pstooltip = $.fn.tooltip;
         initAlerts();
         initToolTips();
         initSearchBar();
+        initInputFileChangeEvent();
       });
     },
 

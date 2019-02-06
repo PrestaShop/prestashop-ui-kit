13cf6d16baf808914137643a1ca37042a96b3825
Tomas Ilginis <tilginis@gmail.com>
2019-02-06 14:04:33 +0200

    splits input text retrieval in another function

--- js/events/file-input.js
+++ js/events/file-input.js
@@ -1,20 +1,20 @@
+const getTextByFiles = ($label, files) => {
+    const filesCount = files.length;
+
+    if (1 === filesCount) {
+        return files[0].name;
+    }
+
+    //todo: pluralization
+    return '';
+}
+
 export const initInputFileChangeEvent = () => {
     jQuery('.custom-file-input').on('change', function (event) {
         const $input = $(event.currentTarget);
         const files = $input[0].files;
-        const filesCount = files.length;
         const $label = $input.next('label');
 
-        if (1 === filesCount) {
-            $label.text(files[0].name);
-        } else if (1 < filesCount) {
-            const multipleFilesTranslationText = $input.attr('data-multiple-files-text');
-
-            if (typeof multipleFilesTranslationText !== 'undefined') {
-                $label.text(filesCount + ' ' + multipleFilesTranslationText);
-            } else {
-                $label.text(filesCount + ' files');
-            }
-        }
+        $label.text(getTextByFiles($label, files));
     })
 }
